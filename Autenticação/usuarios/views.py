f3e48ffd3e222b7e74c5e0b7125bbe46064596a5
Roosch <85234531+enzorooschqueiroz@users.noreply.github.com>
2024-04-02 12:40:32 -0300

    Criação de superusuário e validação de usuário

--- Autenticação/usuarios/views.py
+++ Autenticação/usuarios/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 def cadastro (request):
-    return render (request, "cadastro.html")
+    if request.method == "GET":
+        return render (request, "cadastro.html")
+    else:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = User.objects.get(username=username)
+
+        if user:
+            return HttpResponse("Já existe um usuário com esse username")
+
+        return HttpResponse(username)
+    
+    
+
 
 def login (request):
     return render (request, "login.html")
