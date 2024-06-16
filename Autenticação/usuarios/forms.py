639d0ee21f54769ac163769e57ab3036731b2f6b
Roosch <85234531+enzorooschqueiroz@users.noreply.github.com>
2024-06-16 00:15:33 -0300

    finalizando

--- Autenticação/usuarios/forms.py
+++ Autenticação/usuarios/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class SimulacaoForm(forms.Form):
-    media = forms.FloatField(label='Média', initial=3.0, min_value=0.1, max_value=10.0)
-    desvio_padrao = forms.FloatField(label='Desvio Padrão', initial=1.0, min_value=0.1, max_value=5.0)
-    numero_simulacoes = forms.IntegerField(label='Número de Simulações', initial=1000000, min_value=1000, max_value=10000000)
+    media_tempo = forms.FloatField(label='Média de Tempo')
+    desvio_padrao_tempo = forms.FloatField(label='Desvio Padrão de Tempo')
+    numero_simulacoes = forms.IntegerField(label='Número de Simulações')
--- Autenticação/usuarios/views.py
+++ Autenticação/usuarios/views.py
@@ -1,26 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
-from django.contrib.auth import login as login_django
+from django.contrib.auth import authenticate, login as login_django, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.contrib.auth import logout
 from .models import MonteCarloContent
+from .forms import SimulacaoForm
 import numpy as np
 import matplotlib.pyplot as plt
 from io import BytesIO
 import base64
-from .forms import SimulacaoForm
 
-def cadastro (request):
+def cadastro(request):
     if request.method == "GET":
-        return render (request, "cadastro.html")
+        return render(request, "cadastro.html")
     else:
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password')
+        
         user = User.objects.filter(username=username).first()
-
         if user:
             return HttpResponse("Já existe um usuário com esse username")
         
@@ -33,52 +31,49 @@ def cadastro (request):
             return redirect("plataforma")
         else:
             return HttpResponse("Erro ao autenticar o usuário após o cadastro")
-    
 
-def login (request):
+def login(request):
     if request.method == "GET":
-        return render (request, "login.html")
+        return render(request, "login.html")
     else:
         username = request.POST.get('username')
         password = request.POST.get('password')
 
         user = authenticate(username=username, password=password)
-
         if user:
             login_django(request, user)
-            return render (request, "plataforma.html")
+            return redirect("plataforma")
         else:
             return HttpResponse("Email ou senha inválidos")
 
 @login_required(login_url="")
 def plataforma(request):
-    if request.user.is_authenticated:
-        return render (request, "plataforma.html")
-    else:
-        return HttpResponse("Você deve estar registrado...")
-    
-def logout_view(request):
-    logout(request)
-    return redirect('login') 
+    return render(request, "plataforma.html")
+
+def simular_tempo_de_viagem(numero_simulacoes, media_tempo, desvio_padrao_tempo):
+    # Simulação de Monte Carlo
+    distribuicao_tempo = np.random.normal(media_tempo, desvio_padrao_tempo, numero_simulacoes)
+    return distribuicao_tempo
+
 
 def simulacao_view(request):
     if request.method == 'POST':
         form = SimulacaoForm(request.POST)
         if form.is_valid():
-            media = form.cleaned_data['media']
-            desvio_padrao = form.cleaned_data['desvio_padrao']
+            media_tempo = form.cleaned_data['media_tempo']
+            desvio_padrao_tempo = form.cleaned_data['desvio_padrao_tempo']
             numero_simulacoes = form.cleaned_data['numero_simulacoes']
-            
-            # Simulação de Monte Carlo
-            distribuicao = np.random.normal(media, desvio_padrao, numero_simulacoes)
+
+            # Simulação de Monte Carlo para tempo de viagem
+            distribuicao_tempo = simular_tempo_de_viagem(numero_simulacoes, media_tempo, desvio_padrao_tempo)
             
             # Criando o histograma
             plt.figure(figsize=(11, 5.5))
-            plt.hist(distribuicao, bins=50, density=True, alpha=0.75)
-            plt.axvline(np.mean(distribuicao), color='r', linestyle='dashed', linewidth=1)
-            plt.xlabel('Valores')
+            plt.hist(distribuicao_tempo, bins=50, density=True, alpha=0.75)
+            plt.axvline(np.mean(distribuicao_tempo), color='r', linestyle='dashed', linewidth=1)
+            plt.xlabel('Tempo de Viagem (minutos)')
             plt.ylabel('Densidade')
-            plt.title('Simulação de Monte Carlo')
+            plt.title('Simulação de Tempo de Viagem')
             plt.grid(True)
             
             # Convertendo o gráfico para imagem base64 para ser exibido no template
@@ -95,13 +90,14 @@ def simulacao_view(request):
             return render(request, 'simulacao.html', {'form': form, 'plot_url': plot_url})
     
     else:
-        # Se o método for GET, simplesmente renderize o formulário
         form = SimulacaoForm()
     
     return render(request, 'simulacao.html', {'form': form})
 
+@login_required(login_url="")
 def equipe_view(request):
     equipe_members = [
+      
         'Enzo Queiroz',
         'Gabriel Barbosa',
         'Paulo Araújo',
@@ -116,3 +112,7 @@ def equipe_view(request):
 def home(request):
     montecarlo_contents = MonteCarloContent.objects.all()
     return render(request, 'home.html', {'montecarlo_contents': montecarlo_contents})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
